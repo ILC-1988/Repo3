53a6c1c87d0402ec912b62a44dfef693a65e8c98
Илья Черницкий <ilc@Mac-mini-Ila.local>
2023-03-01 16:11:40 +0300

    second commit

---
+++ Repo3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Repo3
+//
+//  Created by Илья Черницкий on 1.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hell World")
+    }
+
+
+}
+
